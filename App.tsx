1c916738fef3e9c1b6511d37d547aa22082f72c4
Alejandro Gil <ajgp8821@gmail.com>
2023-05-18 23:23:24 -0300

    Installation and configuration Drawer

--- App.tsx
+++ App.tsx
@@ -3,20 +3,22 @@ import * as React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { StackNavigator } from './src/navigator/StackNavigator'
+import { BasicSideMenu } from './src/navigator/BasicSideMenu'
 
 const App = () => {
 	return (
 		// <View style={styles.container}>
 		<NavigationContainer>
-			<StackNavigator />
+			{/* <StackNavigator /> */}
+			<BasicSideMenu />
 		</NavigationContainer>
 	)
 }
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-	}
-})
+// const styles = StyleSheet.create({
+// 	container: {
+// 		flex: 1,
+// 	}
+// })
 
 export default App
