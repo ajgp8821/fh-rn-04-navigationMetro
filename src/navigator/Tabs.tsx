d55ccb9e46df651a9e5a75bc0fa600468fb61949
Alejandro Gil <ajgp8821@gmail.com>
2023-05-21 15:19:41 -0300

    Bottom Tab Navigator

--- src/navigator/Tabs.tsx
+++ src/navigator/Tabs.tsx
@@ -1,23 +1,59 @@
 import React from 'react'
+import { Text } from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { Tab1Screen } from '../screens/Tab1Screen'
 import { Tab2Screen } from '../screens/Tab2Screen'
-import { Tab3Screen } from '../screens/Tab3Screen'
+// import { Tab3Screen } from '../screens/Tab3Screen'
+import { StackNavigator } from './StackNavigator'
+import { colors } from '../theme/appTheme'
 
 export type StackTabsParams = {
   Tab1Screen: undefined,
   Tab2Screen: undefined,
-  Tab3Screen: undefined,
+  StackNavigator: undefined,
 }
 
 const Tab = createBottomTabNavigator<StackTabsParams>()
 
 export const Tabs = () => {
 	return (
-		<Tab.Navigator>
-			<Tab.Screen name="Tab1Screen" component={Tab1Screen} />
-			<Tab.Screen name="Tab2Screen" component={Tab2Screen} />
-			<Tab.Screen name="Tab3Screen" component={Tab3Screen} />
+		<Tab.Navigator
+			sceneContainerStyle={{
+				backgroundColor: 'white'
+			}}
+			screenOptions={ ({route}) => ({
+				tabBarActiveTintColor: colors.primary,
+				tabBarStyle: {
+					borderTopColor: colors.primary,
+					borderTopWidth: 0,
+					elevation: 0
+				},
+				tabBarLabelStyle: {
+					fontSize: 15
+				},
+				tabBarIcon: ({color, focused, size}) => {
+					console.log(route)
+
+					let iconName = ''
+					switch (route.name) {
+					case 'Tab1Screen':
+						iconName = 'T1'
+						break
+					case 'Tab2Screen':
+						iconName = 'T2'
+						break
+					case 'StackNavigator':
+						iconName = 'T3'
+						break
+					}
+
+					return <Text style={{color}}>{iconName}</Text>
+				}
+			})}
+		>
+			<Tab.Screen name="Tab1Screen" options={{title:'Tab1'}} component={Tab1Screen} />
+			<Tab.Screen name="Tab2Screen" options={{title:'Tab2'}} component={Tab2Screen} />
+			<Tab.Screen name="StackNavigator" options={{title:'Stack'}} component={StackNavigator} />
 		</Tab.Navigator>
 	)
 }
--- src/theme/appTheme.tsx
+++ src/theme/appTheme.tsx
@@ -1,5 +1,10 @@
 import { StyleSheet } from 'react-native'
 
+export const colors = {
+	// primary: '#00BCD4',
+	primary: '#5856D6',
+}
+
 export const style = StyleSheet.create({
 	globalMargin: {
 		marginHorizontal: 20
